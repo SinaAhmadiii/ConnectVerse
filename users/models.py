b7a424ad5d2bf4def050a9ed9d738f1cb46bb817
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-12 10:39:58 +0330

    feat:add UserProfile class

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField(User, related_name="friends")
+    followers = models.ManyToManyField(User, related_name="followers")
+    following = models.ManyToManyField(User, related_name="following")
+    archived_posts = models.ManyToManyField('posts.Post',related_name="archived_posts")
+    profile_picture = models.ImageField(upload_to="profile_pictures")
+    bio = models.TextField()
+
+
+    def __str__(self) -> str:
+        return  self.user.username
