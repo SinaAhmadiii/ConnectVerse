5b98c67fccc6bc8b81d7d97e1984a298550e4400
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-12 10:59:52 +0330

    feat: add post class

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    category_tags = models.ManyToManyField("categories.Category")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    liked_by = models.ManyToManyField(User, related_name="liked_posts")
+    disliked_by = models.ManyToManyField(User, related_name="disliked_posts")
+
+
+    def __str__(self):
+        return self.title
+    
