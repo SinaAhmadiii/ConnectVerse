34314041c374ec5c93435d973066b0a8c885cd6b
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 12:36:31 +0330

    feat: add comment class

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
+from post.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+
+    def __str__(self):
+        return f"Comment {self.comment_id} by {self.user.username} on Post {self.post.post_id}"
