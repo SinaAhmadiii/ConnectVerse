67ba31b5840f32c0a7608d3da61ab06313ec657b
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 12:42:17 +0330

    feat: add comment def

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Comment
 
-# Create your views here.
+def comment_detail(request, comment_id):
+    comment = Comment.objects.get(comment_id=comment_id)
+    return render(request, 'comment_detail.html', {'comment': comment})
