e35175a9364180b4825697e4e199a5a3040b8b05
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 12:08:52 +0330

    fix : fix some codes

--- info/views.py
+++ info/views.py
@@ -3,4 +3,4 @@ from .models import Profile
 
 def profile_detail(request, user_id):
     profile = Profile.objects.get(user_id=user_id)
-    return render(request, 'info/profile_detail.html', {'profile': profile})
+    return render(request, 'profile_detail.html', {'profile': profile})
