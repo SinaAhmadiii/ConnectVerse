2d75ab32f12bd13e081b4b3e3c4622ab63670cc9
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 11:53:45 +0330

    feat: add some feature

--- info/views.py
+++ info/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+def profile_detail(request, user_id):
+    profile = Profile.objects.get(user_id=user_id)
+    return render(request, 'info/profile_detail.html', {'profile': profile})
