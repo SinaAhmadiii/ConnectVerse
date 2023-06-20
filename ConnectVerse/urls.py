984ec42b1d4ef42bf4b530e5bb5a2e108fe2b482
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 11:34:39 +0330

    fix: fix some code

--- ConnectVerse/urls.py
+++ ConnectVerse/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    # Other URL patterns for your project
+    
+   
 ]
