9398a05d50dd406f0cadd0e48e2da3a54fe73b48
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 12:25:53 +0330

    feat: add def

--- post/views.py
+++ post/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_detail(request, post_id):
+    post = Post.objects.get(post_id=post_id)
+    return render(request, 'post_detail.html', {'post': post})
