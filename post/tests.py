ac54a9d7d49ec8b75191fdc7ae6eeaf03bfa5f40
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 12:32:56 +0330

    Test: add test for post

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import RequestFactory
+from django.urls import reverse
+import pytest
+from .models import Post
+from .views import post_detail
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_post_detail_view():
+    post = Post.objects.create(post_id=1, user_id=1, post_text='Test post', likes_count=0, views_count=0)
+    request = RequestFactory().get(reverse('post:post_detail', args=[post.post_id]))
+    response = post_detail(request, post_id=post.post_id)
+    assert response.status_code == 200
+    assert 'post' in response.context
+    assert 'post/post_detail.html' in response.template_name
