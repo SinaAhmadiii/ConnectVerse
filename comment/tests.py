964b3d5878ba2eaa840d1ceb673f7d187ced11d2
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 12:53:29 +0330

    Test: test for comment

--- comment/tests.py
+++ comment/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import RequestFactory
+from django.urls import reverse
+import pytest
+from .models import Comment
+from .views import comment_detail
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_comment_detail_view():
+    comment = Comment.objects.create(comment_id=1, user_id=1, post_id=1, comment_text='Test comment')
+    request = RequestFactory().get(reverse('comment:comment_detail', args=[comment.comment_id]))
+    response = comment_detail(request, comment_id=comment.comment_id)
+    assert response.status_code == 200
+    assert 'comment' in response.context
+    assert 'comment/comment_detail.html' in response.template_name
