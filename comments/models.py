6792795a3731b5a9f1cf754bd90243b3881c1442
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 10:30:22 +0330

    fix: fix some codes

--- comments/models.py
+++ comments/models.py
@@ -6,6 +6,7 @@ class Comment(models.Model):
     content = models.TextField()
     post = models.ForeignKey("posts.Post", on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reply_to = models.ForeignKey("self",blank=True,null=True)
 
 
     def __str__(self):
--- users/models.py
+++ users/models.py
@@ -1,16 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
 
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    friends = models.ManyToManyField(User, related_name="friends")
-    followers = models.ManyToManyField(User, related_name="followers")
-    following = models.ManyToManyField(User, related_name="following")
-    archived_posts = models.ManyToManyField('posts.Post',related_name="archived_posts")
-    profile_picture = models.ImageField(upload_to="profile_pictures")
-    bio = models.TextField()
-
-
-    def __str__(self) -> str:
-        return  self.user.username
