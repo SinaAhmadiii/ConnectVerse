27483e76367923c76c1f5b49c8c31cf04ac03c07
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 12:18:13 +0330

    feat: add post class

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_text = models.TextField()
+    likes_count = models.IntegerField(default=0)
+    views_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Post {self.post_id} by {self.user.username}"
