1b672f86df7814fd676a3c524c370cd5b6721655
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 13:16:03 +0330

    feat: add some def

--- reply/views.py
+++ reply/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Reply
+from comment.models import Comment
 
-# Create your views here.
+def reply_detail(request, reply_id):
+    reply = get_object_or_404(Reply, reply_id=reply_id)
+    return render(request, 'reply_detail.html', {'reply': reply})
+
+def comment_replies(request, comment_id):
+    comment = get_object_or_404(Comment, comment_id=comment_id)
+    replies = Reply.objects.filter(comment=comment)
+    return render(request, 'comment_replies.html', {'comment': comment, 'replies': replies})
