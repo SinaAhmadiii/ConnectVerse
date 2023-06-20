2a7994b5f80d96b726ddaf48ba36446ed54b4455
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 13:33:33 +0330

    feat: add bookmark model

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
+from post.models import Post
 
-# Create your models here.
+class Bookmark(models.Model):
+    bookmark_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Bookmark ID: {self.bookmark_id}"
