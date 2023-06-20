7dd2b9a2f43ba15672f16f1aad61ba4d00082886
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 13:00:07 +0330

    feat: add model

--- reply/models.py
+++ reply/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
+from comment.models import Comment
 
-# Create your models here.
+class Reply(models.Model):
+    reply_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    reply_text = models.TextField()
+
+    def __str__(self):
+        return f"Reply {self.reply_id} by {self.user.username} on Comment {self.comment.comment_id}"
