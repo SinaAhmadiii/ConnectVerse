7974192eb6063a65abc5fd3516ea4e28ac74592f
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 10:58:45 +0330

    Test: tests for register and login

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import Client
+import pytest
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_register_view():
+    client = Client()
+    response = client.post('/users/register/', {
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'username': 'johndoe',
+        'email': 'johndoe@example.com',
+        'age': 25,
+        'phone_number': '1234567890',
+        'password': 'password123',
+    })
+    assert response.status_code == 302  
+    assert response.url == '/users/login/'  
+
+@pytest.mark.django_db
+def test_login_view():
+    client = Client()
+    response = client.post('/users/login/', {
+        'email': 'johndoe@example.com',
+        'password': 'password123',
+    })
+    assert response.status_code == 302  
+    assert response.url == '/home/'  
+    
