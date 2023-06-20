efae8389868dd9ac2ec24291e64dc55906648f3c
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-20 11:45:31 +0330

    feat: add models

--- info/models.py
+++ info/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures')
+    account_created = models.DateTimeField(auto_now_add=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Profile for {self.user.username}"
