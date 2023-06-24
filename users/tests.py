8103f1cd7f5e81766e20fbce23e71e690a028860
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-24 11:31:53 +0330

    fix: fix a code

--- users/tests.py
+++ users/tests.py
@@ -1,28 +1,36 @@
-from django.test import Client
-import pytest
+from django.test import TestCase
+from .models import User
 
-@pytest.mark.django_db
-def test_register_view():
-    client = Client()
-    response = client.post('/users/register/', {
-        'first_name': 'John',
-        'last_name': 'Doe',
-        'username': 'johndoe',
-        'email': 'johndoe@example.com',
-        'age': 25,
-        'phone_number': '1234567890',
-        'password': 'password123',
-    })
-    assert response.status_code == 302  
-    assert response.url == '/users/login/'  
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'age': 25,
+            'phone_number': '1234567890',
+            'password': 'password123'
+        }
 
-@pytest.mark.django_db
-def test_login_view():
-    client = Client()
-    response = client.post('/users/login/', {
-        'email': 'johndoe@example.com',
-        'password': 'password123',
-    })
-    assert response.status_code == 302  
-    assert response.url == '/home/'  
-    
+    def test_full_name(self):
+        user = User(**self.user_data)
+        full_name = user.full_name()
+        expected_full_name = 'John Doe'
+        self.assertEqual(full_name, expected_full_name)
+
+    def test_set_password(self):
+        user = User(**self.user_data)
+        new_password = 'newpassword456'
+        user.set_password(new_password)
+        self.assertEqual(user.password, new_password)
+
+    def test_get_user_by_email_exists(self):
+        user = User(**self.user_data)
+        user.save()
+        retrieved_user = User.get_user_by_email('johndoe@example.com')
+        self.assertEqual(retrieved_user, user)
+
+    def test_get_user_by_email_does_not_exist(self):
+        retrieved_user = User.get_user_by_email('nonexistent@example.com')
+        self.assertIsNone(retrieved_user)
