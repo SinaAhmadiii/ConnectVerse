ca260c66ff2c27caf828a6e3e00c5b29d78a7c71
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-24 17:35:19 +0330

    fix: fix comment class

--- comment/models.py
+++ comment/models.py
@@ -3,10 +3,22 @@ from users.models import User
 from post.models import Post
 
 class Comment(models.Model):
-    comment_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment_text = models.TextField()
+    comment_id = models.AutoField(primary_key=True, verbose_name='Comment ID')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Post')
+    comment_text = models.TextField(verbose_name='Comment Text', help_text='Enter your comment here.')
 
     def __str__(self):
         return f"Comment {self.comment_id} by {self.user.username} on Post {self.post.post_id}"
+
+    def get_comment_id(self):
+        return self.comment_id
+
+    def get_user(self):
+        return self.user
+
+    def get_post(self):
+        return self.post
+
+    def get_comment_text(self):
+        return self.comment_text
