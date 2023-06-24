9b271c95ae2f023251db34df009b83d103fc406b
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-24 11:49:29 +0330

    fix: fix some test

--- users/tests.py
+++ users/tests.py
@@ -1,36 +1,31 @@
 from django.test import TestCase
 from .models import User
 
-class UserTestCase(TestCase):
+class UserTest(TestCase):
     def setUp(self):
-        self.user_data = {
-            'first_name': 'John',
-            'last_name': 'Doe',
-            'username': 'johndoe',
-            'email': 'johndoe@example.com',
-            'age': 25,
-            'phone_number': '1234567890',
-            'password': 'password123'
-        }
+        self.user = User.objects.create(
+            first_name="John",
+            last_name="Doe",
+            username="johndoe",
+            email="johndoe@example.com",
+            age=25,
+            phone_number="1234567890",
+        )
 
     def test_full_name(self):
-        user = User(**self.user_data)
-        full_name = user.full_name()
-        expected_full_name = 'John Doe'
-        self.assertEqual(full_name, expected_full_name)
+        full_name = self.user.full_name()
+        self.assertEqual(full_name, "John Doe")
 
     def test_set_password(self):
-        user = User(**self.user_data)
-        new_password = 'newpassword456'
-        user.set_password(new_password)
-        self.assertEqual(user.password, new_password)
+        new_password = "newpassword"
+        self.user.set_password(new_password)
+        self.assertTrue(self.user.check_password(new_password))
 
-    def test_get_user_by_email_exists(self):
-        user = User(**self.user_data)
-        user.save()
-        retrieved_user = User.get_user_by_email('johndoe@example.com')
-        self.assertEqual(retrieved_user, user)
+    def test_get_user_by_email(self):
+        email = "johndoe@example.com"
+        user = User.get_user_by_email(email)
+        self.assertEqual(user, self.user)
 
-    def test_get_user_by_email_does_not_exist(self):
-        retrieved_user = User.get_user_by_email('nonexistent@example.com')
-        self.assertIsNone(retrieved_user)
+    def test_string_representation(self):
+        string_representation = str(self.user)
+        self.assertEqual(string_representation, "johndoe")
