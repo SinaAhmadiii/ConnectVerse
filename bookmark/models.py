fee1cfdfd1fe66e42510347e837f408da7bb36c8
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-24 19:14:06 +0330

    fix: fix bookmark class

--- bookmark/models.py
+++ bookmark/models.py
@@ -3,9 +3,19 @@ from users.models import User
 from post.models import Post
 
 class Bookmark(models.Model):
-    bookmark_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    bookmark_id = models.AutoField(primary_key=True, verbose_name='Bookmark ID')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User', help_text='The user who bookmarked the post')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Post', help_text='The post that was bookmarked')
 
     def __str__(self):
         return f"Bookmark ID: {self.bookmark_id}"
+
+    def get_user_bookmarks(self, user):
+        return Bookmark.objects.filter(user=user)
+
+    def get_bookmarked_posts(self, user):
+        return Post.objects.filter(bookmark__user=user)
+
+    def bookmark_post(self, user, post):
+        bookmark = Bookmark(user=user, post=post)
+        bookmark.save()
