eb736fa602f94c93e51fc53930095e2ada62c873
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-24 17:28:26 +0330

    fix: fix test of info

--- info/tests.py
+++ info/tests.py
@@ -1,19 +1,31 @@
-from django.test import RequestFactory
-from django.urls import reverse
-import pytest
-from .models import Profile
-from .views import profile_detail
+from django.test import TestCase
+from users.models import User
+from models import Profile
 
-@pytest.mark.django_db
-def test_profile_detail_view():
-    profile = Profile.objects.create(user_id=1, profile_picture='profile.jpg', bio='Test Bio')
+class ProfileModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', email='test@example.com')
+        self.profile = Profile.objects.create(user=self.user)
 
-    request = RequestFactory().get(reverse('info:profile_detail', args=[profile.user_id]))
+    def test_get_full_name(self):
+        full_name = self.profile.get_full_name()
+        self.assertEqual(full_name, self.user.get_full_name())
 
-    response = profile_detail(request, user_id=profile.user_id)
+    def test_get_username(self):
+        username = self.profile.get_username()
+        self.assertEqual(username, self.user.username)
 
-    assert response.status_code == 200
+    def test_update_profile_picture(self):
+        new_picture = 'path/to/new_picture.jpg'
+        self.profile.update_profile_picture(new_picture)
+        self.assertEqual(self.profile.profile_picture, new_picture)
 
-    assert 'profile' in response.context
+    def test_add_bio(self):
+        new_bio = 'This is a new bio.'
+        self.profile.add_bio(new_bio)
+        self.assertEqual(self.profile.bio, new_bio)
 
-    assert 'profile_detail.html' in response.template_name
+    def test_delete_profile(self):
+        self.profile.delete_profile()
+        self.assertFalse(Profile.objects.filter(user=self.user).exists())
+        self.assertFalse(User.objects.filter(username='testuser').exists())
