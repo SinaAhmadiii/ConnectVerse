f202184eff7367cf7b76f44aa601c1232b35388f
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-24 11:27:13 +0330

    feat: add some method

--- users/models.py
+++ users/models.py
@@ -10,3 +10,22 @@ class User(models.Model):
     phone_number = models.CharField(max_length=20)
     password = models.CharField(max_length=255)
 
+    def full_name(self):
+        """Returns the full name of the user."""
+        return f"{self.first_name} {self.last_name}"
+
+    def set_password(self, new_password):
+        """Sets a new password for the user."""
+        self.password = new_password
+        self.save()
+
+    def get_user_by_email(email):
+        """Retrieves a user instance based on the given email."""
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
+
+    def __str__(self):
+        """Returns a string representation of the user."""
+        return self.username
