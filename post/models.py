4ffe63878ea146f28e8b49c64ae959b468e631f2
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-24 17:21:06 +0330

    fix :fix post class

--- post/models.py
+++ post/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from users.models import User
 
 class Post(models.Model):
-    post_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_text = models.TextField()
-    likes_count = models.IntegerField(default=0)
-    views_count = models.IntegerField(default=0)
+    post_id = models.AutoField(primary_key=True, verbose_name='Post ID')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+    post_text = models.TextField(verbose_name='Post Text', help_text='Enter the text for the post.')
+    likes_count = models.IntegerField(default=0, verbose_name='Likes Count')
+    views_count = models.IntegerField(default=0, verbose_name='Views Count')
 
     def __str__(self):
         return f"Post {self.post_id} by {self.user.username}"
+
+    def increment_likes(self):
+        self.likes_count += 1
+        self.save()
+
+post = Post.objects.get(post_id=1)
+post.increment_likes()
+
