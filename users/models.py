e51371a038717aeba5365b009c211aeb6d6c1a2e
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-24 17:04:42 +0330

    fix: fix user class

--- users/models.py
+++ users/models.py
@@ -2,12 +2,40 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    age = models.IntegerField(null=True, blank=True)
-    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(
+        max_length=255,
+        verbose_name='First Name',
+        help_text='The user\'s first name.'
+    )
+    last_name = models.CharField(
+        max_length=255,
+        verbose_name='Last Name',
+        help_text='The user\'s last name.'
+    )
+    username = models.CharField(
+        unique=True,
+        max_length=255,
+        verbose_name='Username',
+        help_text='The unique username for the user.'
+    )
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Email',
+        help_text='The unique email address for the user.'
+    )
+    age = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name='Age',
+        help_text='The age of the user (optional).'
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+        verbose_name='Phone Number',
+        help_text='The phone number of the user (optional).'
+    )
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -16,6 +44,7 @@ class User(AbstractUser):
         self.set_password(new_password)
         self.save()
 
+    @staticmethod
     def get_user_by_email(email):
         try:
             return User.objects.get(email=email)
