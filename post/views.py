dfbb924dfd481fb796652721a14024d031854fa0
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-06-27 23:45:43 +0330

    fix: fix some codes

--- post/views.py
+++ post/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
-def post_detail(request, post_id):
-    post = Post.objects.get(post_id=post_id)
-    return render(request, 'post_detail.html', {'post': post})
+def create_post(request):
+    if request.method == 'POST':
+        post_text = request.POST.get('post_text')
+        post = Post(user=request.user, post_text=post_text)
+        post.save()
+        return redirect('home')
+    
+    return render(request, 'post/create_post.html')
+
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, post_id=post_id)
+    
+    if request.method == 'POST':
+        post_text = request.POST.get('post_text')
+        post.post_text = post_text
+        post.save()
+        return redirect('home')
+    
+    context = {'post': post}
+    return render(request, 'post/edit_post.html', context)
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, post_id=post_id)
+    
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+    
+    context = {'post': post}
+    return render(request, 'post/delete_post.html', context)
