adbd31e900f43a91730626693aed0561ef58daae
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-07-03 16:57:22 +0330

    feat: add urls

--- info/urls.py
+++ info/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from . import views
+from .views import (
+    ProfileCreateView,
+    ProfileUpdateView,
+    BioUpdateView,
+    ProfileDeleteView,
+    ProfileDetailView,
+)
 
 app_name = 'info'
 
 urlpatterns = [
-    path('profile/<int:user_id>/', views.profile_detail, name='profile_detail'),
+    path('profile/create/', ProfileCreateView.as_view(), name='profile_create'),
+    path('profile/update/', ProfileUpdateView.as_view(), name='profile_update'),
+    path('bio/update/', BioUpdateView.as_view(), name='bio_update'),
+    path('profile/delete/', ProfileDeleteView.as_view(), name='profile_delete'),
+    path('profile/', ProfileDetailView.as_view(), name='profile_detail'),
 ]
