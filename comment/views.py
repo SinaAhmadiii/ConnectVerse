64680a94eb2802292102f5ba4817c0946ce3515f
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-07-04 19:36:29 +0330

    feat:Update code with PEP8

--- comment/views.py
+++ comment/views.py
@@ -3,24 +3,28 @@ from django.urls import reverse_lazy
 from .models import Comment
 from .forms import CommentForm
 
+
 class CommentListView(ListView):
     model = Comment
-    template_name = 'comment/comment_list.html'  
-    context_object_name = 'comments'  
+    template_name = 'comment/comment_list.html'
+    context_object_name = 'comments'
+
 
 class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentForm
-    template_name = 'comment/comment_create.html'  
-    success_url = reverse_lazy('comment_list')  
+    template_name = 'comment/comment_create.html'
+    success_url = reverse_lazy('comment_list')
+
 
 class CommentUpdateView(UpdateView):
     model = Comment
     form_class = CommentForm
-    template_name = 'comment/comment_update.html'  
-    success_url = reverse_lazy('comment_list')  
+    template_name = 'comment/comment_update.html'
+    success_url = reverse_lazy('comment_list')
+
 
 class CommentDeleteView(DeleteView):
     model = Comment
-    template_name = 'comment/comment_delete.html'  
-    success_url = reverse_lazy('comment_list')  
+    template_name = 'comment/comment_delete.html'
+    success_url = reverse_lazy('comment_list')
